bc0717c8e797895d601028bbe871a430730ef7e2
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-01-07 19:16:59 -0800

    first

---
+++ babyMeasurement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  babyMeasurement
+//
+//  Created by Grimshaw, Aidan T on 1/7/20.
+//  Copyright © 2020 Grimshaw, Aidan T. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
