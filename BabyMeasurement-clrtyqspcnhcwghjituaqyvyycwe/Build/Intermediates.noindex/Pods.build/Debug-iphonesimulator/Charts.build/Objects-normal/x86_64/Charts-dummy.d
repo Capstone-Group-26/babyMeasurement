5138f4d5f10bf3bb486caff65122d68e452b5d2d
user171419 <user171419@74-80-245-22.ic2net.net>
2020-04-26 21:40:21 -0700

    cleaned up

--- BabyMeasurement-clrtyqspcnhcwghjituaqyvyycwe/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Charts.build/Objects-normal/x86_64/Charts-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/user171419/Documents/babyMeasurement/Pods/Target\ Support\ Files/Charts/Charts-dummy.m \
-  /Users/user171419/Documents/babyMeasurement/Pods/Target\ Support\ Files/Charts/Charts-prefix.pch
--- BabyMeasurement-clrtyqspcnhcwghjituaqyvyycwe/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-test2.build/Objects-normal/x86_64/Pods-test2-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/user171419/Documents/babyMeasurement/BabyMeasurement-clrtyqspcnhcwghjituaqyvyycwe/Build/Products/Debug-iphonesimulator/Charts/Charts.modulemap \
-  /Users/user171419/Documents/babyMeasurement/Pods/Target\ Support\ Files/Pods-test2/Pods-test2-dummy.m
